083f1ae2333b3300d6495188329ab83c70d32bc5
Jorge Contreras <jcontreras@asna.com>
2023-10-13 12:58:23 -0500

    Reorganized Order fields using Web Tabs.

---
+++ SunFarmSite/wwwroot/js/RecordTabs.js
@@ -0,0 +1,21 @@
+﻿const openTab = (tabNavButton) => {
+    const tabPage = document.getElementById(tabNavButton.innerHTML);
+
+    if (tabPage) {
+        // Get all the elements with class="tabcontent" and hide them
+        const tabcontent = document.querySelectorAll('[class~="tabcontent"]');
+        for (let i = 0, l = tabcontent.length; i < l; i++) {
+            tabcontent[i].style.display = 'none';
+        }
+
+        // Get all elements with class="tablinks" and remove the class "active"
+        const tablinks = document.querySelectorAll('[class~="tablinks"]');
+        for (let i = 0, l = tablinks.length; i < l; i++) {
+            tablinks[i].className = tablinks[i].className.replace(' active', '');
+        }
+
+        // Show the current tab, and add an "active" class to the button that opened the tab.
+        tabPage.style.display = 'block';
+        tabNavButton.className += ' active';
+    }
+}
